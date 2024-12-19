19530dc6a3bc3a09ce09d06a8b104c54e0914f9f
Sushil <sushil.webexp@gmail.com>
2024-12-19 18:18:34 +0530

    change

--- src/component/Validation/loginvalidation.jsx
+++ src/component/Validation/loginvalidation.jsx
@@ -3,4 +3,4 @@ import * as Yup from "yup";
 export const loginvalidation = Yup.object().shape({
   email: Yup.string().email("Invalid email address").required("Email is required"),
   password: Yup.string().required("Password is required"),
-});
+})
